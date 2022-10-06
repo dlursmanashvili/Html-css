cec281f905e523dd42062a0bbf0f14a3a9ef069d
dlursmanashvili <66787923+dlursmanashvili@users.noreply.github.com>
2022-10-06 21:10:50 +0400

    project html css

---
+++ htm&css/project 1/produqtebi.js
@@ -0,0 +1,119 @@
+const photo1_div1 = document.querySelector('.photo1_div1')
+const photo1 = document.querySelector(".photo1")
+const suratis_chasma_div = document.querySelector(".suratis_gamotana_div")
+const vertikaluri_suratebis_fanjrebi = document.querySelector('.vertikaluradsuratebi')
+photo1_div1.onclick = function() {
+    const chasasveli_photo = photo1;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+    divi_sadac_unda_chavsva_surati.removeChild(photo3);
+
+}
+
+
+const photo1_div2 = document.querySelector('.photo1_div2')
+const photo2 = document.querySelector(".photo2")
+photo1_div2.onclick = function() {
+    const chasasveli_photo = photo2;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo3);
+
+}
+
+
+
+const photo1_div3 = document.querySelector('.photo1_div3')
+const photo3 = document.querySelector(".photo3")
+photo1_div3.onclick = function() {
+
+    const chasasveli_photo = photo3;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+
+}
+
+
+
+const photo1_div4 = document.querySelector('.photo1_div4')
+const photo4 = document.querySelector(".photo4")
+photo1_div4.onclick = function() {
+
+    const chasasveli_photo = photo4;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+
+}
+
+
+
+const photo1_div5 = document.querySelector('.photo1_div5')
+const photo5 = document.querySelector(".photo5")
+photo1_div5.onclick = function() {
+
+    const chasasveli_photo = photo5;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+
+}
+
+const photo1_div6 = document.querySelector('.photo1_div6')
+const photo6 = document.querySelector(".photo6")
+photo1_div6.onclick = function() {
+
+    const chasasveli_photo = photo6;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+
+}
+
+
+
+
+
+const photo1_div7 = document.querySelector('.photo1_div7')
+const photo7 = document.querySelector(".photo7")
+photo1_div7.onclick = function() {
+
+    const chasasveli_photo = photo7;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+
+}
+
+
+const photo1_div8 = document.querySelector('.photo1_div8')
+const photo8 = document.querySelector(".photo8")
+photo1_div8.onclick = function() {
+
+    const chasasveli_photo = photo8;
+    const divi_sadac_unda_chavsva_surati = suratis_chasma_div;
+    divi_sadac_unda_chavsva_surati.appendChild(chasasveli_photo)
+    chasasveli_photo.classList.add('mtavarisurati')
+    divi_sadac_unda_chavsva_surati.removeChild(photo1);
+    divi_sadac_unda_chavsva_surati.removeChild(photo2);
+    // if (divi_sadac_unda_chavsva_surati.appendChild(photo1) === true) {
+
+    //     divi_sadac_unda_chavsva_surati.removeChild(photo2);
+    // }
+
+}
